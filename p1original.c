838380f4dd9ce1283c1e30592992040686550041
Shetty10 <97089508+Shettyv10@users.noreply.github.com>
2022-02-09 04:31:56 +0000

    ..

--- p1original.c
+++ p1original.c
@@ -1,2 +1,24 @@
+#include<stdio.h>
+#include<math.h>
 
+void input(int *base,int *height)
+{
+ printf("enter base and height\n");
+  scanf("%d%d" ,base ,height);
+}
+void find_area(int base, int height, int *area)
+{
+   *area=0.5*base*height;
+}
+void output(int base, int height, int area)
+{
+  printf("the area of the triangle whose base and height are %d %d is =%d \n",base,height,area);
+  }
+int main()
+{
+  int base,height,area;
+  input(&base,&height);
+  find_area(base,height,&area);
+  output(base,height,area);
+  return 0;
 }
